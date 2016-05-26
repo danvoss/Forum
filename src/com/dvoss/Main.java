d15f211e7835ee7abdf5d7e25b24a073f10a36ac
Dan <danielandrewvoss@gmail.com>
2016-05-26 11:42:53 -0400

    Refactor

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -7,10 +7,9 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static ArrayList<Post> parsePosts(String filename) throws FileNotFoundException {
         ArrayList<Post> posts = new ArrayList<>();
-
-        File f = new File("posts.txt");
+        File f = new File(filename);
         Scanner fileScanner = new Scanner(f);
         while (fileScanner.hasNext()) {
             String line = fileScanner.nextLine();
@@ -18,21 +17,29 @@ public class Main {
             Post post = new Post(Integer.valueOf(columns[0]), columns[1], columns[2]);
             posts.add(post);
         }
+        return posts;
+    }
+
+    public static void printPosts(ArrayList<Post> posts, int currentPost) {
+        int postId = 0;
+        for (Post post : posts) {
+            if (post.replyId == currentPost) {
+                System.out.printf("[%s] %s by %s\n", postId, post.text, post.author);
+            }
+            postId++;
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Post> posts = parsePosts("posts.txt");
 
         Scanner consoleScanner = new Scanner(System.in);
         int currentPost = -1;
         while (true) {
-            int postId = 0;
-            for (Post post : posts) {
-                if (post.replyId == currentPost) {
-                    System.out.printf("[%s] %s by %s\n", postId, post.text, post.author);
-                }
-                postId++;
-            }
+            printPosts(posts, currentPost);
 
             System.out.println("Type the ID you want to see replies to:");
             currentPost = Integer.valueOf(consoleScanner.nextLine());
-
         }
     }
 }
